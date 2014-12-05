23244f96cb02dbcd258105322aabef7d5e0d3f56
Johan Larsson <johan_gavle1@hotmail.com>
2014-12-05 19:43:21 +0100

    Updated assembly version

--- Gu.Wpf.ToolTips/Properties/AssemblyInfo.cs
+++ Gu.Wpf.ToolTips/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
 [assembly: GuidAttribute("12DC3603-3E01-4180-B7FE-32C5BD5B5217")]
