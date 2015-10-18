772d2d03fec7b2a4d1681199bc4d24eabf0af9ec
Johan Larsson <johan_gavle1@hotmail.com>
2015-10-18 13:19:15 +0200

    Refactor keys.

--- Gu.Wpf.ToolTips/PopupButton.Keys.cs
+++ Gu.Wpf.ToolTips/PopupButton.Keys.cs
@@ -1,28 +1,19 @@
 ﻿namespace Gu.Wpf.ToolTips
 {
-    using System.Collections.Generic;
     using System.Runtime.CompilerServices;
     using System.Windows;
 
     public partial class PopupButton
     {
-        private static readonly Dictionary<string, ComponentResourceKey> Cache = new Dictionary<string, ComponentResourceKey>();
+        public static ResourceKey TextOverlayTemplateKey { get; } = CreateKey();
 
-        public static ResourceKey TextOverlayTemplateKey => Get();
+        public static ResourceKey DefaultOverlayTemplateKey { get; } = CreateKey();
 
-        public static ResourceKey DefaultOverlayTemplateKey => Get();
+        public static ResourceKey MissingToolTipOverlayKey { get; } = CreateKey();
 
-        public static ResourceKey MissingToolTipKey => Get();
-
-        private static ComponentResourceKey Get([CallerMemberName] string caller = null)
+        private static ComponentResourceKey CreateKey([CallerMemberName] string caller = null)
         {
-            ComponentResourceKey key;
-            if (!Cache.TryGetValue(caller, out key))
-            {
-                key = new ComponentResourceKey(typeof(PopupButton), caller);
-                Cache.Add(caller, key);
-            }
-            return key;
+            return new ComponentResourceKey(typeof(PopupButton), caller);
         }
     }
 }
