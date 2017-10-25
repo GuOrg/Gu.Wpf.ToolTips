2f1cd52c8e40a7a0792fb41e9bc999e424892ad4
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-25 12:14:09 +0200

    UI-test project.

---
+++ Gu.Wpf.ToolTips.UiTests/MainWindowTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Gu.Wpf.ToolTips.UiTests
+{
+    using Gu.Wpf.UiAutomation;
+    using NUnit.Framework;
+
+    public class MainWindowTests
+    {
+        [Test]
+        public void ClickAllTabs()
+        {
+            // Just a smoke test so we don't crash.
+            using (var app = Application.Launch("Gu.Wpf.ToolTips.Demo.exe"))
+            {
+                var window = app.MainWindow;
+                var tab = window.FindTabControl();
+                foreach (var tabItem in tab.Items)
+                {
+                    tabItem.Click();
+                }
+            }
+        }
+    }
+}
---
+++ Gu.Wpf.ToolTips.UiTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gu.Wpf.ToolTips.UiTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Johan Larsson")]
+[assembly: AssemblyProduct("Gu.Wpf.ToolTips.UiTests")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b553a962-a81d-403d-a2ad-55aed77ccda2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
