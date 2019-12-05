ae3b8ec9f425362c96db4b0bd53181c7f27edda3
Johan Larsson <johan_gavle1@hotmail.com>
2019-12-05 22:16:36 +0100

    pack with nuget.

---
+++ Gu.Wpf.ToolTips/AssemblyIAttributes.cs
@@ -0,0 +1,6 @@
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("http://Gu.com/ToolTips", "Gu.Wpf.ToolTips")]
+[assembly: XmlnsPrefix("http://Gu.com/ToolTips", "toolTips")]
