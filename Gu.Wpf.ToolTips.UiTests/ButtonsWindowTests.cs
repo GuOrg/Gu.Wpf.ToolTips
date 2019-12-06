decfdab4cf3233bdf38dd9cd957b346aa300ec6a
JohanLarsson <johan_gavle1@hotmail.com>
2019-12-06 09:03:11 +0100

    More UI-tests.

--- Gu.Wpf.ToolTips.UiTests/ButtonsWindowTests.cs
+++ Gu.Wpf.ToolTips.UiTests/ButtonsWindowTests.cs
@@ -3,13 +3,13 @@ namespace Gu.Wpf.ToolTips.UiTests
     using Gu.Wpf.UiAutomation;
     using NUnit.Framework;
 
-    public class ButtonsWindowTests
+    public static class ButtonsWindowTests
     {
         private const string ExeFileName = "Gu.Wpf.ToolTips.Demo.exe";
         private const string WindowName = "ButtonsWindow";
 
         [OneTimeSetUp]
-        public void OneTimeSetUp()
+        public static void OneTimeSetUp()
         {
             ImageAssert.OnFail = OnFail.SaveImageToTemp;
         }
@@ -19,7 +19,8 @@ namespace Gu.Wpf.ToolTips.UiTests
         {
             using var app = Application.AttachOrLaunch(ExeFileName, WindowName);
             var window = app.MainWindow;
-            window.FindCheckBox("Adorned element visible").IsChecked = true;
+            window.FindCheckBox("Buttons enabled").IsChecked = false;
+            window.FindCheckBox("Buttons visible").IsChecked = true;
             window.FindCheckBox("IsOverlayVisible (null uses default behavior)").IsChecked = null;
             window.WaitUntilResponsive();
         }
@@ -31,13 +32,28 @@ namespace Gu.Wpf.ToolTips.UiTests
         }
 
         [Test]
-        public void AdornedElementVisibility()
+        public static void ButtonVisible()
         {
             using var app = Application.AttachOrLaunch(ExeFileName, WindowName);
             var window = app.MainWindow;
-            window.FindCheckBox("Adorned element visible").IsChecked = false;
-            window.FindCheckBox("Adorned element visible").IsChecked = true;
-            ImageAssert.AreEqual("Button_with_default_touch_tool_tip.png", window.FindButton("Button with touch tooltip"));
+            ImageAssert.AreEqual("Button_with_default_touch_tool_tip.png", window.FindButton("Button with touch tool tip"));
+            window.FindCheckBox("Buttons visible").IsChecked = false;
+            window.FindCheckBox("Buttons visible").IsChecked = true;
+            ImageAssert.AreEqual("Button_with_default_touch_tool_tip.png", window.FindButton("Button with touch tool tip"));
+        }
+
+        [Test]
+        public static void ButtonEnabled()
+        {
+            using var app = Application.AttachOrLaunch(ExeFileName, WindowName);
+            var window = app.MainWindow;
+            ImageAssert.AreEqual("Button_with_default_touch_tool_tip.png", window.FindButton("Button with touch tool tip"));
+
+            window.FindCheckBox("Buttons enabled").IsChecked = true;
+            ImageAssert.AreEqual("Button_with_default_touch_tool_tip_hidden.png", window.FindButton("Button with touch tool tip"));
+
+            window.FindCheckBox("Buttons enabled").IsChecked = false;
+            ImageAssert.AreEqual("Button_with_default_touch_tool_tip.png", window.FindButton("Button with touch tool tip"));
         }
     }
 }
