69f9c378adf8f8edb20f2f32c6064615327105f3
JohanLarsson <johan_gavle1@hotmail.com>
2019-12-06 15:05:08 +0100

    Try explicit colours.

--- Gu.Wpf.ToolTips.UiTests/WindowWithoutExplicitAdornerDecoratorTests.cs
+++ Gu.Wpf.ToolTips.UiTests/WindowWithoutExplicitAdornerDecoratorTests.cs
@@ -3,10 +3,11 @@ namespace Gu.Wpf.ToolTips.UiTests
     using Gu.Wpf.UiAutomation;
     using NUnit.Framework;
 
-    public static class ButtonsWindowTests
+    [Ignore("tbd")]
+    public static class WindowWithoutExplicitAdornerDecoratorTests
     {
         private const string ExeFileName = "Gu.Wpf.ToolTips.Demo.exe";
-        private const string WindowName = "ButtonsWindow";
+        private const string WindowName = "WindowWithoutExplicitAdornerDecorator";
 
         [OneTimeSetUp]
         public static void OneTimeSetUp()
@@ -32,6 +33,17 @@ namespace Gu.Wpf.ToolTips.UiTests
         }
 
         [Test]
+        public static void ButtonVisible()
+        {
+            using var app = Application.AttachOrLaunch(ExeFileName, WindowName);
+            var window = app.MainWindow;
+            ImageAssert.AreEqual("Button_with_default_touch_tool_tip.png", window.FindButton("Button with touch tool tip"));
+            window.FindCheckBox("Buttons visible").IsChecked = false;
+            window.FindCheckBox("Buttons visible").IsChecked = true;
+            ImageAssert.AreEqual("Button_with_default_touch_tool_tip.png", window.FindButton("Button with touch tool tip"));
+        }
+
+        [Test]
         public static void ButtonEnabled()
         {
             using var app = Application.AttachOrLaunch(ExeFileName, WindowName);
@@ -42,17 +54,6 @@ namespace Gu.Wpf.ToolTips.UiTests
             ImageAssert.AreEqual("Button_with_default_touch_tool_tip_hidden.png", window.FindButton("Button with touch tool tip"));
 
             window.FindCheckBox("Buttons enabled").IsChecked = false;
-            ImageAssert.AreEqual("Button_with_default_touch_tool_tip.png", window.FindButton("Button with touch tool tip"));
-        }
-
-        [Test]
-        public static void ButtonVisible()
-        {
-            using var app = Application.AttachOrLaunch(ExeFileName, WindowName);
-            var window = app.MainWindow;
-            ImageAssert.AreEqual("Button_with_default_touch_tool_tip.png", window.FindButton("Button with touch tool tip"));
-            window.FindCheckBox("Buttons visible").IsChecked = false;
-            window.FindCheckBox("Buttons visible").IsChecked = true;
             ImageAssert.AreEqual("Button_with_default_touch_tool_tip.png", window.FindButton("Button with touch tool tip"));
         }
     }
