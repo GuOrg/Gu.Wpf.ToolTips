df5f816c75c55d86f1a17dc05fbcfd005413eea2
JohanLarsson <johan_gavle1@hotmail.com>
2019-12-31 13:40:24 +0100

    Update DebugWindow

---
+++ Gu.Wpf.ToolTips.Demo/Windows/DebugWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Gu.Wpf.ToolTips.Demo.Windows
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public partial class DebugWindow : Window
+    {
+        public DebugWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_ToolTipOpening(object sender, ToolTipEventArgs e)
+        {
+        }
+
+        private void Button_ToolTipClosing(object sender, ToolTipEventArgs e)
+        {
+        }
+    }
+}
