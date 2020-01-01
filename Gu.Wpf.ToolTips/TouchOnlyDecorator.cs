e851e5f25a273d98f5e78f913b07edb0c5b13855
JohanLarsson <johan_gavle1@hotmail.com>
2020-01-01 14:25:22 +0100

    TouchOnlyDecorator handle all preview moouse events.

--- Gu.Wpf.ToolTips/TouchOnlyDecorator.cs
+++ Gu.Wpf.ToolTips/TouchOnlyDecorator.cs
@@ -1,8 +1,10 @@
 ﻿namespace Gu.Wpf.ToolTips
 {
     using System.Diagnostics;
+    using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
+    using System.Windows.Markup;
 
     /// <summary>
     /// A decorator that is not hit test visible for mouse.
@@ -15,6 +17,12 @@
         static TouchOnlyDecorator()
 #pragma warning restore CA1810 // Initialize reference type static fields inline
         {
+            Handle(Mouse.PreviewMouseMoveEvent);
+            Handle(Mouse.PreviewMouseDownEvent);
+            Handle(Mouse.PreviewMouseDownOutsideCapturedElementEvent);
+            Handle(Mouse.PreviewMouseUpEvent);
+            Handle(Mouse.PreviewMouseWheelEvent);
+
             InputManager.Current.PreProcessInput += (sender, args) =>
             {
                 if (args.StagingItem.Input is { RoutedEvent: { Name: "PreviewStylusInRange" } })
@@ -36,14 +44,19 @@
                     currentInput = InputType.None;
                 }
 
-                //Dump("PostProcessInput", args);
+                Dump("PostProcessInput", args);
             };
+
+            static void Handle(RoutedEvent previewMouseEvent)
+            {
+                EventManager.RegisterClassHandler(typeof(TouchOnlyDecorator), previewMouseEvent, new RoutedEventHandler((o, e) => e.Handled = true));
+            }
 
             static void Dump(string name, NotifyInputEventArgs args)
             {
                 if (args.StagingItem.Input is { } inputEventArgs)
                 {
-                    Debug.WriteLine($"{name,-16} {inputEventArgs.GetType().Name,-21} {inputEventArgs.Device?.GetType().Name ?? "null",-21} {inputEventArgs.Device?.Target?.ToString() ?? "null",-21} {inputEventArgs.RoutedEvent.Name}");
+                    //Debug.WriteLine($"{name,-16} {inputEventArgs.GetType().Name,-21} {inputEventArgs.Device?.GetType().Name ?? "null",-21} {inputEventArgs.Device?.Target?.ToString() ?? "null",-21} {inputEventArgs.RoutedEvent.Name}");
                     // Debug.WriteLine($"{name} RoutedEvent: {inputEventArgs.RoutedEvent.Name} Device: {inputEventArgs.Device?.GetType().Name ?? "null"} Source: {inputEventArgs.Source} currentInput: {currentInput}");
                 }
             }
