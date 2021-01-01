97dbc357f6dd33abd3c62152c74baf3f5f862f94
JohanLarsson <johan_gavle1@hotmail.com>
2021-01-01 21:46:47 +0100

    [assembly: CLSCompliant(false)]

--- Gu.Wpf.ToolTips/AssemblyIAttributes.cs
+++ Gu.Wpf.ToolTips/AssemblyIAttributes.cs
@@ -1,5 +1,8 @@
+﻿using System;
 using System.Windows;
 using System.Windows.Markup;
+
+[assembly: CLSCompliant(false)]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("http://Gu.com/ToolTips", "Gu.Wpf.ToolTips")]
